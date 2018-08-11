30fe1eba0d6102de7a39102b9e15d783243b64fe
vlaunyc <vlau.nyc@gmail.com>
2018-08-10 22:28:46 -0400

    Initial Commit.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <div>yoda's bubble, response</div>
+
+        <div>yoda's face</div>
+
+        <div>textbox for user</div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+// Hot Module Replacement (HMR) is a tool to reload your application in the browser
+if (module.hot) {
+  module.hot.accept();
+}
